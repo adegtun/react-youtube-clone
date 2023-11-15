07419b904c76e81469d201aec95bb65afe471af7
Temitope Adegunle <tadegunle@axxess.com>
2023-11-15 13:46:00 +0100

    my first check-in

---
+++ src/utils/fetchFromApi.js
@@ -0,0 +1,26 @@
+//const axios = require("axios");
+import axios from "axios";
+
+const Base_URL = "https://youtube-v31.p.rapidapi.com";
+const options = {
+  url: Base_URL,
+  params: {
+    maxResults: "50",
+  },
+  headers: {
+    "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
+    "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
+  },
+};
+
+// try {
+//   const respo = await axios.request(options);
+//   console.log(response.data);
+// } catch (error) {
+//   console.error(error);
+// }
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${Base_URL}/${url}`, options);
+  return data;
+};
